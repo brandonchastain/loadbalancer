4ea47d5adb77b3256e9c45f19ff03145de4075c7
Brandon Chastain <brandonchastain@gmail.com>
2024-05-17 20:39:14 -0700

    fix url parsing and redirects

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,9 +1,9 @@
 from bottle import route, run, request, response, redirect
 import requests
 import configparser
+from urllib.parse import urlparse
 
-server = 'http://localhost' #, 'https://shorturl.brandonchastain.com']
-serverports = []
+serverlist = None
 currentServer = 0
 srcIpMap = dict()
 
@@ -13,41 +13,42 @@ def index(p=""):
     method = request.method
     body = request.body.read()
     headers = dict(request.headers)
+
     print("%s %s ->" % (method, request.url))
     url = getServer(request.environ.get("REMOTE_ADDR")) + '/' + p
-    #headers['Host'] = servers[0][8:]
-    headers['Host'] = server[7:]
-    print("%s %s" % (method, url))
-    print("%s" % (body))
+    print("%s" % (url))
+
+    headers['Host'] = urlparse(url).netloc
+
     r = requests.request(method=method, url=url, data=body, headers=headers, allow_redirects=False)
-    print(r.status_code)
-    print(r.headers)
+
     response.status = r.status_code
     for key in r.headers:
         response.headers[key] = r.headers[key]
-    response.body = r.content
-
+    return r.content
 
 def getServer(srcIp):
     global currentServer
     global srcIpMap
     if srcIp not in srcIpMap:
-        srcIpMap[srcIp] = serverports[currentServer]
-    currentServer = (currentServer + 1) % len(serverports)
-    return server + ':' + srcIpMap[srcIp]
+        srcIpMap[srcIp] = serverlist[currentServer]
+        currentServer = (currentServer + 1) % len(serverlist)
+    return srcIpMap[srcIp]
 
 def getServerRoundRobin():
     global currentServer
-    res = server + ':' + serverports[currentServer]
-    currentServer = (currentServer + 1) % len(serverports)
+    res = serverlist[currentServer]
+    currentServer = (currentServer + 1) % len(serverlist)
     return res
 
 def main():
     config = configparser.ConfigParser()
     config.read('config.ini')
     port = config['default']['port']
-    global serverports
-    serverports = config['default']['serverports'].split(',')
+
+    global serverlist
+    serverlist = config['default']['serverlist'].split(',')
+
     run(host='0.0.0.0', port=port, server="gunicorn")
 
 if __name__ == "__main__":
